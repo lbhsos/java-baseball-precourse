e7a41b302a6c2dbfd01e4847c25e44386ac9242f
lbhsos <bohyunlee.dev@gmail.com>
2022-04-17 19:53:31 +0900

    feat: Change start function in BaseballGame
    Change to call private functions at the start function,
    so that we can just call start method in BaseballGame to run game
    
    Delete the BaseballGameTest code because it's useless
    Since we can test whether the game works normally in ApplicationTest code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.domain.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+        boolean onGame = true;
+        while (onGame) {
+            onGame = baseballGame.start();
+        }
     }
 }
--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -5,16 +5,31 @@ import java.util.Map;
 public class BaseballGame {
 
     private Computer computer;
+    private Player player;
     private boolean correctAnswer = false;
     private boolean onGame = true;
-    private static final int CONTINUE = 1;
 
-    void start() {
+    private static final int CONTINUE = 1;
+    public static final String BASEBALL_INPUT_REQUIRED_MESSAGE = "숫자를 입력해주세요: ";
+    public static final String GAME_CONTINUE_INPUT_REQUIRED_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public boolean start() {
         this.computer = new Computer();
+
+        while (!correctAnswer) {
+            System.out.print(BASEBALL_INPUT_REQUIRED_MESSAGE);
+            Map<Integer, Integer> baseballInput = player.getBaseballInput();
+            correctAnswer = isCorrectAnswer(baseballInput);
+        }
+
+        System.out.println(GAME_CONTINUE_INPUT_REQUIRED_MESSAGE);
+        int gameContinueInput = player.getGameContinueInput();
+        boolean continueFlag = checkContinue(gameContinueInput);
+        return continueFlag;
     }
 
-    //TODO userInput 받기전 value 검증 필요
-    boolean checkContinue(int userInput) {
+    private boolean checkContinue(int userInput) {
         if (userInput == CONTINUE) {
             this.correctAnswer = false;
             return this.onGame;
@@ -22,12 +37,11 @@ public class BaseballGame {
         return this.onGame = false;
     }
 
-    boolean isCorrectAnswer(Map<Integer, Integer> userBaseballInput) {
+    private boolean isCorrectAnswer(Map<Integer, Integer> userBaseballInput) {
         boolean result = computer.compare(userBaseballInput);
+        if (result) {
+            System.out.println(GAME_END_MESSAGE);
+        }
         return this.correctAnswer = result;
-    }
-
-    public void quit() {
-        System.exit(0);
     }
 }
