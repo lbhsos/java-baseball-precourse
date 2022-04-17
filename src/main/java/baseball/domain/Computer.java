8433cef3776db7a8e809200601b0d77251c4715c
lbhsos <bohyunlee.dev@gmail.com>
2022-04-17 16:22:47 +0900

    feat: Implementing random generator method for Computer

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,19 +1,30 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Computer {
-    // Map<positionIndex, number>
+
     private Map<Integer, Integer> baseballNum;
+
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private static final int BASEBALL_NUM = 3;
 
     Computer() {
         this.generator();
     }
 
     private void generator() {
+        Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
         baseballNum = new HashMap<>();
-        //TODO
+
+        for (int seq = 0; seq<BASEBALL_NUM; seq++) {
+            int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+            baseballNum.put(seq, randomNumber);
+        }
     }
 
     boolean compare(Map<Integer, Integer> userBaseballInput) {
