ba23161ca9a57dde49738ae934e8923764fe999a
lbhsos <bohyunlee.dev@gmail.com>
2022-04-18 22:06:37 +0900

    test: Add test not to return a string, when it is 0

--- src/test/java/baseball/model/BaseballResultTest.java
+++ src/test/java/baseball/model/BaseballResultTest.java
@@ -91,4 +91,29 @@ public class BaseballResultTest {
         assertThat(resultText).isEqualTo(givenBallCount+"볼 "+givenStrikeCount+"스트라이크");
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("결과 호출시 Baseball 객체의 strikeCount가 0이면 스트라이크 문자열은 포함하지 않는다.")
+    void get_strike_zero_result(int givenBallCount) {
+        // given
+        int givenStrikeCount = 0;
+        givenBaseballResult = new BaseballResult(givenStrikeCount, givenBallCount);
+        // when
+        String resultText = givenBaseballResult.getResultText();
+        // then
+        assertThat(resultText.trim()).isEqualTo(givenBallCount+"볼");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("결과 호출시 Baseball 객체의 ballCount가 0이면 스트라이크 문자열은 포함하지 않는다.")
+    void get_ball_zero_result(int givenStrikeCount) {
+        // given
+        int givenBallCount = 0;
+        givenBaseballResult = new BaseballResult(givenStrikeCount, givenBallCount);
+        // when
+        String resultText = givenBaseballResult.getResultText();
+        // then
+        assertThat(resultText).isEqualTo(givenStrikeCount+"스트라이크");
+    }
 }
