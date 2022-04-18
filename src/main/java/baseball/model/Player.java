f4d596292d007b473a970ab6bf4f396ea2f19321
lbhsos <bohyunlee.dev@gmail.com>
2022-04-18 21:58:52 +0900

    refactor: Change order

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -15,29 +15,19 @@ public class Player {
     private static final int BASEBALL_INPUT_LENGTH = 3;
 
     public Map<Integer, Integer> getBaseballInput() {
-        String input = readLine();
-        return stringToMap(input.trim());
+        String input = readLine().trim();
+        checkLength(input, BASEBALL_INPUT_LENGTH);
+        return stringToMap(input);
     }
 
     public int getGameContinueInput() {
-        String input = readLine();
-        int inGameStatus = isInGameStatus(input.trim());
+        String input = readLine().trim();
+        checkLength(input, GAME_INPUT_LENGTH);
+        int inGameStatus = isInGameStatus(input);
         return inGameStatus;
     }
 
-    private int isInGameStatus(String input) {
-        checkLength(input, GAME_INPUT_LENGTH);
-        char userInput = input.charAt(READ_ONLY_ONE);
-        isNumeric(input.charAt(READ_ONLY_ONE));
-        int numericValue = Character.getNumericValue(userInput);
-        if (numericValue != GameStatus.EXIT.getValue() && numericValue != GameStatus.CONTINUE.getValue()) {
-            throw new IllegalArgumentException(INVALID_VALUE_GAME_CONTINUE_INPUT_MESSAGE);
-        }
-        return numericValue;
-    }
-
     private Map<Integer, Integer> stringToMap(String numberAsText) {
-        checkLength(numberAsText, BASEBALL_INPUT_LENGTH);
         Map<Integer, Integer> baseballNum = new HashMap<>();
         for (int i=0; i<BASEBALL_INPUT_LENGTH; i++) {
             char c = numberAsText.charAt(i);
@@ -50,16 +40,14 @@ public class Player {
         return baseballNum;
     }
 
-    private void isInRange(int numericValue) {
-        if (numericValue == 0) {
-            throw new IllegalArgumentException(INVALID_RANGE_INPUT_MESSAGE);
+    private int isInGameStatus(String input) {
+        char userInput = input.charAt(READ_ONLY_ONE);
+        isNumeric(input.charAt(READ_ONLY_ONE));
+        int numericValue = Character.getNumericValue(userInput);
+        if (numericValue != GameStatus.EXIT.getValue() && numericValue != GameStatus.CONTINUE.getValue()) {
+            throw new IllegalArgumentException(INVALID_VALUE_GAME_CONTINUE_INPUT_MESSAGE);
         }
-    }
-
-    private void checkDuplicate(Map<Integer, Integer> baseballNum, int numKey) {
-        if (baseballNum.containsKey(numKey)) {
-            throw new IllegalArgumentException(DIFFERENT_VALUE_REQUIRED_MESSAGE);
-        }
+        return numericValue;
     }
 
     private void checkLength(String numberAsText, int fixedLength ) {
@@ -71,6 +59,18 @@ public class Player {
     private void isNumeric(char charNumber) {
         if (!Character.isDigit(charNumber)) {
             throw new IllegalArgumentException(INVALID_VALUE_INPUT_MESSAGE);
+        }
+    }
+
+    private void isInRange(int numericValue) {
+        if (numericValue == 0) {
+            throw new IllegalArgumentException(INVALID_RANGE_INPUT_MESSAGE);
+        }
+    }
+
+    private void checkDuplicate(Map<Integer, Integer> baseballNum, int numKey) {
+        if (baseballNum.containsKey(numKey)) {
+            throw new IllegalArgumentException(DIFFERENT_VALUE_REQUIRED_MESSAGE);
         }
     }
 }
